ea9faf74385c75da604b430be6213768416c0617
wtucker <wtbtucker@gmail.com>
2025-03-13 17:56:28 -0400

    NBA stats will return stats for the current day when requesting stats for a future date. This means some tables could have out of date statistics created before the corresponding date occurred. Therefore, the conditional check if a table exists is not sufficient for updating data. Change logic to fetch all games for the current season up to and including the current day

--- src/Process-Data/Get_Data.py
+++ src/Process-Data/Get_Data.py
@@ -16,25 +16,29 @@ url = config['data_url']
 
 con = sqlite3.connect("../../Data/TeamData.sqlite")
 
+today = datetime.today().date()
+
 for key, value in config['get-data'].items():
+    if key != "2024-25":
+        continue
+
     date_pointer = datetime.strptime(value['start_date'], "%Y-%m-%d").date()
     end_date = datetime.strptime(value['end_date'], "%Y-%m-%d").date()
 
-    while date_pointer <= end_date:
+    while date_pointer <= end_date and date_pointer <= today:
+        table_name = date_pointer.strftime("%Y-%m-%d")
+
         print("Getting data: ", date_pointer)
 
         raw_data = get_json_data(
             url.format(date_pointer.month, date_pointer.day, value['start_year'], date_pointer.year, key))
         df = to_data_frame(raw_data)
 
-        date_pointer = date_pointer + timedelta(days=1)
-
         df['Date'] = str(date_pointer)
 
-        df.to_sql(date_pointer.strftime("%Y-%m-%d"), con, if_exists="replace")
+        df.to_sql(table_name, con, if_exists="replace")
 
-        time.sleep(random.randint(1, 3))
+        date_pointer = date_pointer + timedelta(days=1)
 
-        # TODO: Add tests
 
 con.close()
