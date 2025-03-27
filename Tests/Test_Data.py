bf3cc8b64c8451c5007251589efebfbaa0744027
wtucker <wtbtucker@gmail.com>
2025-03-26 21:08:56 -0400

    First attempt at testing

--- Tests/Test_Data.py
+++ Tests/Test_Data.py
@@ -9,9 +9,13 @@ query = "SELECT name FROM sqlite_master WHERE type='table';"
 # Execute the query and fetch the results
 tables = con.execute(query).fetchall()
 
+column_query = f"PRAGMA table_info('2025-03-12');"
+columns = con.execute(column_query).fetchall()
+print([col[1] for col in columns])
 # Print the table names
-for table in tables:
-    print(table[0])
+query = f"SELECT * FROM '2025-03-13';"
+rows = con.execute(query).fetchall()[0]
+print(rows)
 
 # Close the connection
 con.close()
